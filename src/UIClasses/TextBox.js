36b0f3634bdc5521b6035e6a07995b28f170ae0c
Antonio Ferreira <profissionalantoniofferreira@gmail.com>
2023-07-25 17:49:52 -0300

    Elemento de caixa de texto

---
+++ src/UIClasses/TextBox.js
@@ -0,0 +1,30 @@
+const TAG =  "gameTextBox";
+
+export class TextBox{
+    text;
+    element;
+    Visible = false;
+
+    setText(string){
+        this.text = string;
+        this.element.textContent = this.text;
+    }
+
+    setVisible(){
+        this.element = document.createElement(TAG);
+        this.element.textContent = this.text;
+        let div = document.getElementById("forCanva");
+        div.appendChild(this.element);
+        //gameCanva.style = "";
+        this.Visible = true;
+    }
+
+    setUnvisble(){
+        this.element.remove();
+        this.Visible = false;
+    }
+
+    getVisible(){
+        return this.Visible;
+    }
+}
